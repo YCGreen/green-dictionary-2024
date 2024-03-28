b5f2a5abac4989e0795e1eb0525c1fdfc32963af
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-03-28 13:53:31 -0400

    fixed class, added tests

--- src/main/java/green/dictionary/EnglishDictionary.java
+++ src/main/java/green/dictionary/EnglishDictionary.java
@@ -2,46 +2,20 @@ package green.dictionary;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class EnglishDictionary {
-    /*
-    Reads the englishDictionary file once (in the constructor)
-     */
+    private static final HashMap<String, List<String[]>> definitions = new HashMap<>();
 
-    private static List<String[]> text;
-    private static FileReader fileReader;
-    private static CSVReader csvReader;
-    private static HashMap<String, List<String[]>> definitions = new HashMap<>();
-
-
-    public EnglishDictionary() throws IOException, CsvException {
-      /* fileReader = new FileReader("/Users/yaelgreen/IdeaProjects/green-dictionary-2024/src/main/resources/englishDictionary.csv");
-        csvReader = new CSVReader(fileReader);
-        text = csvReader.readAll();
-
-        for(int i = 0; i < text.size(); i++) {
-            definitions.put(i, text.get(i));
-        } */
+    public EnglishDictionary()  {
         try (CSVReader csvReader = new CSVReader(new FileReader("/Users/yaelgreen/IdeaProjects/green-dictionary-2024/src/main/resources/englishDictionary.csv"))) {
-         /*   String[] prevDef = new String[]{"A"};
-            String[] nextDef;
-            List<String[]> defsPerWord = null;
-
-            while ((nextDef = csvReader.readNext()) != null) {
-                if(nextDef[0].equals(prevDef[0])) {
-                    defsPerWord.add(nextDef);
-                }
-                definitions.put(nextDef[0], defsPerWord);
-                defsPerWord = null;
-                prevDef = nextDef;
-            } */
             String[] nextDef;
             while ((nextDef = csvReader.readNext()) != null) {
                 String word = nextDef[0];
@@ -49,7 +23,8 @@ public class EnglishDictionary {
                 wordDefs.add(nextDef);
                 definitions.put(word, wordDefs);
             }
-            csvReader.close();
+        } catch (IOException | CsvValidationException e) {
+            throw new RuntimeException(e);
         }
 
     }
@@ -58,6 +33,16 @@ public class EnglishDictionary {
         return definitions.get(word);
     }
 
+    public String toString(List<String[]> text) {
+        if(text == null) {
+            return null;
+        }
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < text.size(); i++) {
+            str.append(String.join(", ", text.get(i)));
+        }
+        return str.toString();
+    }
 
 
 
--- src/main/java/green/dictionary/Main.java
+++ src/main/java/green/dictionary/Main.java
@@ -8,25 +8,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        EnglishDictionary dic;
-        try {
-            dic = new EnglishDictionary();
-        //    List<String[]> text = dic.getFullText();
+        EnglishDictionary dic = new EnglishDictionary();
 
-            List<String[]> text = dic.getDefinition("Abandon");
+        List<String[]> text = dic.getDefinition("Abandon");
 
-                for (String[] definition : text) {
-                    System.out.println(String.join(", ", definition));
-                }
-         /*   for (String[] row : text) {
-                for (String cell : row) {
-                    System.out.print(cell + "\t");
-                }
-                System.out.println();
-            } */
-        } catch (IOException | CsvException e) {
-            throw new RuntimeException(e);
+        for (String[] definition : text) {
+            System.out.println(String.join(", ", definition));
         }
+
 
 
     }
